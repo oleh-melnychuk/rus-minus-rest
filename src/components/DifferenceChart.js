fc0fddaf05831784e3999caa7d461120b87c1585
Oleh Melnychuk <oleh.m@uatechpeople.com>
2023-08-03 17:15:33 +0200

    add difference chart comp

---
+++ src/components/DifferenceChart.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { PieChart } from 'react-minimal-pie-chart';
+
+
+const DifferenceChart = () => {
+const [rusData, setRusData] = useState(null)
+
+const totalAmount =  2039758;
+
+useEffect(() => {
+    axios.get('https://russianwarship.rip/api/v2/statistics/latest')
+    .then(function ({data: {data: {stats}}}) {
+      setRusData(stats)
+    })
+}, [])
+
+
+
+
+
+    if(!rusData) return <div>Loading</div>
+
+
+    return(<div className='container'>
+        <h3>Good russians statistics</h3>
+        <p>
+        Good russians - { rusData.personnel_units} <br />
+        Rest of them - {totalAmount - rusData.personnel_units}
+        </p>
+    <PieChart
+    data={[
+      { title: 'Good russians', value: rusData.personnel_units, color: '#E38627' },
+      { title: 'Bad russians', value: totalAmount - rusData.personnel_units, color: '#C13C37' },
+    ]}
+    label={({ dataEntry }) => `${dataEntry.title} - ${Math.round(dataEntry.percentage)} %`}
+    labelPosition={60}
+    
+  />
+  </div>);
+
+}
+
+export default DifferenceChart
